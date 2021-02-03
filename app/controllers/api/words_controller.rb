31fbd1fb1257fca6bcd8227455e5669ef9002cf3
sean <maherseanmilo@gmail.com>
2021-02-02 17:00:11 -0700

    routes and authenticators

--- app/controllers/api/words_controller.rb
+++ app/controllers/api/words_controller.rb
@@ -1,2 +1,10 @@
 class Api::WordsController < ApplicationController
+  def show
+    @word = HTTP
+    .headers({
+      "Authorization" => "Token #{Rails.application.credentials.owlbot_api[:api_key]}"
+    })
+    .get("https://owlbot.info/api/v4/dictionary/ostrich").parse
+    render 'show.json.jb'
+  end
 end
